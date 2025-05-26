e49c547e6c00f7bc724f4cfa69406c8844e1e095
jason4king21 <jason4king21@gmail.com>
2025-05-26 11:32:04 -0500

    Diagram

---
+++ Diagrams/Diagram Generator/architecture.py
@@ -0,0 +1,27 @@
+from diagrams import Diagram, Cluster
+from diagrams.onprem.vcs import Github
+from diagrams.onprem.container import Docker
+from diagrams.onprem.client import Users
+from diagrams.programming.language import Python
+from diagrams.aws.compute import Lambda
+from diagrams.aws.compute import ECR
+from diagrams.aws.security import SecretsManager
+from diagrams.aws.storage import S3
+
+
+with Diagram("Calendly API ETL Pipeline", show=True, filename="diagrams/architecture", outformat="png"):
+    github = Github("GitHub Actions")
+    docker = Docker("Docker Image")
+    ecr = ECR("ECR Repo")
+
+    with Cluster("AWS"):
+        secrets = SecretsManager("Secrets Manager")
+        lambda_fn = Lambda("Lambda (Docker)")
+        s3 = S3("S3 Bucket")
+
+    calendly = Users("Calendly API")
+    python_proc = Python("Python + Pandas")
+
+    github >> docker >> ecr >> lambda_fn
+    lambda_fn >> [secrets, calendly]
+    lambda_fn >> python_proc >> s3
